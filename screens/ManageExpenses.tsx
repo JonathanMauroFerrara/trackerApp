5f26db28ae002a8e00d2ea8f99bea90a8025698c
JonathanDev <jonathanmauro.ferrara@gmail.com>
2023-05-22 22:36:15 +0200

    init project

---
+++ screens/ManageExpenses.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ManageExpenses = () => {
+  return (
+    <View>
+      <Text>ManageExpenses Screen</Text>
+    </View>
+  );
+};
+
+export default ManageExpenses;
